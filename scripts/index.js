8f7bb7e332a210417e405ce656ba50adc162a7da
Ola Christian Gundelsby <lug00ber@gmail.com>
2019-10-22 01:58:09 +0200

    Basic connection to websockets server

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,30 @@
-console.log('heisann verden');
+document.addEventListener('submit', (event) => {
+  if (event.target.dataset && event.target.dataset.type === 'connect') {
+    event.preventDefault();
+
+    const form = event.target;
+    try {
+      const url = new URL(form.elements['url'].value);
+
+      if (url.protocol !== 'ws:') {
+        throw new Error('Only websocket connections allowed', url.href);
+      }
+      // Create WebSocket connection.
+      const socket = new WebSocket(url);
+
+      // Connection opened
+      socket.addEventListener('open', function() {
+        socket.send('Hello Server!');
+      });
+
+      // Listen for messages
+      socket.addEventListener('message', function(event) {
+        console.log('Message from server ', event.data);
+      });
+
+      socket.addEventListener('error', console.log);
+    } catch (err) {
+      console.log('fucked up setting up a connection', err);
+    }
+  }
+});
