9a23e5ce26d8058469e222b1fca710a24fc9c902
Ola Christian Gundelsby <lug00ber@gmail.com>
2024-05-09 22:37:53 +0200

    chore: better logger

--- scripts/util/logger.js
+++ scripts/util/logger.js
@@ -12,46 +12,40 @@ export default function getLogger(id) {
   return {
     log: (...args) => {
       const { message, leftovers } = parseArgs(args);
-      console.log(
-        `%c${id}:`,
-        `color: #${colors.neutral}`,
-        `${message}`,
-        leftovers.length > 0 ? leftovers : undefined,
-      );
+      console.log(...buildArgs(id, colors.neutral, message, leftovers));
     },
     warn: (...args) => {
       const { message, leftovers } = parseArgs(args);
-      console.log(
-        `%c${id}:`,
-        `color: #${colors.warning}`,
-        `${message}`,
-        leftovers.length > 0 ? leftovers : undefined,
-      );
+      console.warn(...buildArgs(id, colors.warning, message, leftovers));
     },
     error: (...args) => {
       const { message, leftovers } = parseArgs(args);
-      console.log(
-        `%c${id}:`,
-        `color: #${colors.error}`,
-        `${message}`,
-        leftovers.length > 0 ? leftovers : undefined,
-      );
+      console.error(...buildArgs(id, colors.error, message, leftovers));
     },
     success: (...args) => {
       const { message, leftovers } = parseArgs(args);
-      console.log(
-        `%c${id}:`,
-        `color: #${colors.success}`,
-        `${message}`,
-        leftovers.length > 0 ? leftovers : undefined,
-      );
+      console.log(...buildArgs(id, colors.success, message, leftovers));
     },
   };
 }
 
 function parseArgs(args) {
-  const message = typeof args[0] === 'string' ? args[0] : '';
+  const message = typeof args[0] === 'string' ? args[0] : null;
   const leftovers = message ? args.slice(1) : args.slice();
 
   return { message, leftovers };
+}
+
+function buildArgs(id, colorCode, message, args) {
+  const logArgs = [`%c${id}:`, `color: #${colorCode}`];
+
+  if (message) {
+    logArgs.push(message);
+  }
+
+  if (args.length > 0) {
+    logArgs.push(...args);
+  }
+
+  return logArgs;
 }
