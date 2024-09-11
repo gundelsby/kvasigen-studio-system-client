5ac03c2bc9a6a5813199207f49ae67afb6ea4cb8
Ola Christian Gundelsby <ola.christian.gundelsby@gmail.com>
2024-09-11 16:04:43 +0200

    feat: part parameter add and update values api methods

--- client/scripts/api/demodata/script/part-parameters.js
+++ client/scripts/api/demodata/script/part-parameters.js
@@ -1,7 +1,15 @@
-import { createPartParameterObject } from '../../../model/demodata/script/part-parameters.js';
 import actionTypes from '../../../state/action-types.js';
+import { createPartParameterObject } from '../../../model/demodata/script/part-parameters.js';
 import { store } from '../../../state/store.js';
 
+export { createPartParameter, updatePartParameterValues };
+
+/**
+ * Create a part parameter object and add it to the data store
+ *
+ * @param {Object} data - input data to create part parameter from
+ * @returns {import('../../../model/demodata/script/part-parameters.js').PartParameter}
+ */
 function createPartParameter(data) {
   const partParameter = createPartParameterObject(data);
   store.dispatch({
@@ -13,9 +21,10 @@ function createPartParameter(data) {
 }
 
 /**
+ * Update values for an existing part parameter datastore object
  *
  * @param {string} uuid
- * @param {import('../../../model/demodata/script/part-parameters.js').PartParameterValue[]} values
+ * @param {import('../../../model/demodata/script/part-parameters.js').PartParameterValue[l]} values
  */
 function updatePartParameterValues(uuid, values) {
   store.dispatch({
